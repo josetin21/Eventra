b4c5852a93aa189da77b789ad53d2bd7b2a2988d
josetin21 <josetin2001@gmail.com>
2026-05-08 21:18:38 +0530

    feat: replace JavaMail with Resend HTTP API for email delivery

--- src/main/java/com/jm/eventra/service/EmailService.java
+++ src/main/java/com/jm/eventra/service/EmailService.java
@@ -8,8 +8,6 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
